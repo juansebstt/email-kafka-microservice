5a1c5fe3387bc90db6f1dc286a2acac5209ee805
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-15 19:25:18 -0500

    first commit

---
+++ src/test/java/com/emailkafkamicroservice/EmailKafkaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailkafkamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailKafkaMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
