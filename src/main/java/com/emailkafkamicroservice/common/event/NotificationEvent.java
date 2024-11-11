fbd87a4f041831d4c479b612e77167017f588372
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-11 02:03:22 -0500

    Implemented NotificationEvent

--- src/main/java/com/emailkafkamicroservice/common/event/NotificationEvent.java
+++ src/main/java/com/emailkafkamicroservice/common/event/NotificationEvent.java
@@ -1,4 +1,14 @@
 package com.emailkafkamicroservice.common.event;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Builder
+@Data
+@ToString
 public class NotificationEvent {
+
+    private String receiverEmail;
+
 }
