3b55d3e47a06dc6b8b3686fde4dedcf21187ee93
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-12 10:04:58 -0500

    Implemented PackageEvent

--- src/main/java/com/emailkafkamicroservice/common/event/PackageEvent.java
+++ src/main/java/com/emailkafkamicroservice/common/event/PackageEvent.java
@@ -1,4 +1,16 @@
 package com.emailkafkamicroservice.common.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PackageEvent {
+
+    private Integer trackingNumber;
+
+    private Boolean isEligible;
+
 }
