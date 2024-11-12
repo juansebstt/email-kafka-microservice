3ddad864931657811222d6ae4d2ddd485cb11e7d
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-12 10:11:09 -0500

    Implemented EventHandler

--- src/main/java/com/emailkafkamicroservice/config/EventHandler.java
+++ src/main/java/com/emailkafkamicroservice/config/EventHandler.java
@@ -1,4 +1,25 @@
 package com.emailkafkamicroservice.config;
 
+import com.emailkafkamicroservice.common.event.LetterEvent;
+import com.emailkafkamicroservice.common.event.NotificationEvent;
+import com.emailkafkamicroservice.common.event.PackageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+@Configuration
 public class EventHandler {
+
+    @Bean
+    public Function<Flux<LetterEvent>, Flux<NotificationEvent>> letterBinding(final LetterEligibleProcessor processor) {
+        return processor;
+    }
+
+    @Bean
+    public Function<Flux<PackageEvent>, Flux<PackageEvent>> packageBinding(final PackageEligibleProcessor processor) {
+        return processor::process;
+    }
+
 }
