807fc0ad1f171963e16f757d4b52a40d0068f71c
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-12 10:38:21 -0500

    Implemented PackageEligibilityService

--- src/main/java/com/emailkafkamicroservice/service/PackageEligibilityService.java
+++ src/main/java/com/emailkafkamicroservice/service/PackageEligibilityService.java
@@ -1,4 +1,8 @@
 package com.emailkafkamicroservice.service;
 
+import com.emailkafkamicroservice.common.event.PackageEvent;
+import reactor.core.publisher.Mono;
+
 public interface PackageEligibilityService {
+    Mono<PackageEvent> eligibilityPackage(PackageEvent packageEvent);
 }
