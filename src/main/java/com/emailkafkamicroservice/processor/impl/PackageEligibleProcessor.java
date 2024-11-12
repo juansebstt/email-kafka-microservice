4c9752ef9978ed6f89d39af5cc32ff76953a5724
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-12 10:45:37 -0500

    Implemented PackageEligibleProcessor

--- src/main/java/com/emailkafkamicroservice/processor/impl/PackageEligibleProcessor.java
+++ src/main/java/com/emailkafkamicroservice/processor/impl/PackageEligibleProcessor.java
@@ -1,4 +1,31 @@
 package com.emailkafkamicroservice.processor.impl;
 
-public class PackageEligibleProcessor {
+import com.emailkafkamicroservice.common.event.PackageEvent;
+import com.emailkafkamicroservice.processor.ProcessorSpec;
+import com.emailkafkamicroservice.service.PackageEligibilityService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@Component
+@Slf4j
+public class PackageEligibleProcessor implements ProcessorSpec<PackageEvent> {
+
+    private final PackageEligibilityService packageEligibilityService;
+
+    @Autowired
+    public PackageEligibleProcessor(PackageEligibilityService packageEligibilityService) {
+        this.packageEligibilityService = packageEligibilityService;
+    }
+
+    @Override
+    public Flux<PackageEvent> process(Flux<PackageEvent> inbound) {
+        return inbound.doOnNext(entryEvent -> log.info("Received event"))
+                .map(this.packageEligibilityService::eligibilityPackage)
+                .map(given -> inbound.blockFirst())
+                .subscribeOn(Schedulers.parallel());
+    }
+
 }
