aad9b2f9b8699429f2d746926a951f519c94b3fa
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-12 11:01:46 -0500

    Finished email-kafka-microservice

--- src/main/java/com/emailkafkamicroservice/service/impl/PackageEligibilityServiceImpl.java
+++ src/main/java/com/emailkafkamicroservice/service/impl/PackageEligibilityServiceImpl.java
@@ -1,4 +1,25 @@
 package com.emailkafkamicroservice.service.impl;
 
-public class PackageEligibilityServiceImpl {
+import com.emailkafkamicroservice.common.event.PackageEvent;
+import com.emailkafkamicroservice.service.PackageEligibilityService;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Service
+public class PackageEligibilityServiceImpl implements PackageEligibilityService {
+
+    @Override
+    public Mono<PackageEvent> eligibilityPackage(PackageEvent packageEvent) {
+        return Mono.just(packageEvent)
+                .flatMap(this::checkIsEligible)
+                .map(given -> packageEvent);
+    }
+
+    private Mono<PackageEvent> checkIsEligible(PackageEvent packageEvent) {
+        return Mono.just(packageEvent)
+                .filter(Objects::nonNull)
+                .map(given -> packageEvent);
+    }
 }
